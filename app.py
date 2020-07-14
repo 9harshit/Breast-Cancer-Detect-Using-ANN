eb8b3d461e3b14e2348f8a4b714e267eee185038
9harshit <43147421+9harshit@users.noreply.github.com>
2020-07-14 11:31:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,31 +8,31 @@ import joblib
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'randonkey'
+app.config['SECRET_KEY'] = 'mysecretkey'
 
-model = Sequential()
-
-# Adding the input layer and the first hidden layer
-model.add(Dense(units = 10, kernel_initializer = 'uniform', activation = 'relu', input_dim = 30))
-
-# Adding the second hidden layer
-model.add(Dense(units = 10, kernel_initializer = 'uniform', activation = 'relu'))
-
-model.add(Dense(units = 10, kernel_initializer = 'uniform', activation = 'relu'))
-
-# Adding the output layer
-model.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
-
-# Compiling the ANN
-model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-
-model = load_model('breast_cancer_detect.h5')
 
 @app.route("/", methods = ["POST", "GET"])  # this sets the route to this page
 def home():
     if request.method == "POST":
         data = np.zeros(30)
         j =0 
+        model = Sequential()
+
+        # Adding the input layer and the first hidden layer
+        model.add(Dense(units = 10, kernel_initializer = 'uniform', activation = 'relu', input_dim = 30))
+
+        # Adding the second hidden layer
+        model.add(Dense(units = 10, kernel_initializer = 'uniform', activation = 'relu'))
+
+        model.add(Dense(units = 10, kernel_initializer = 'uniform', activation = 'relu'))
+
+        # Adding the output layer
+        model.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+        # Compiling the ANN
+        model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+        model = load_model('breast_cancer_detect.h5')
 
         session.permanent = True
         for i in ["radius_mean", "texture_mean","perimeter_mean","area_mean","smoothness_mean","compactness_mean","concavity_mean","concave_points_mean","symmetry_mean","fractal_dimension_mean","radius_se","texture_se","perimeter_se","area_se","smoothness_se","compactness_se","concavity_se","concave_points_se","symmetry_se","fractal_dimension_se","radius_worst","texture_worst","perimeter_worst","area_worst","smoothness_worst","compactness_worst","concavity_worst","concave_points_worst","symmetry_worst","fractal_dimension_worst"]:
@@ -43,7 +43,8 @@ def home():
         sc = joblib.load('detect_scaler.pkl')
         data = data.reshape(1, -1)
 
-        data = sc.transform(data)
+        data = sc.fit(data).transform(data)
+
 
         # Initialising the ANN
     
